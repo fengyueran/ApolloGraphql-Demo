d88a9ecc5a020a74c1c99a20e6c073e788b479e1
xinghunm <xinghunm@curacloudcorp.com>
2018-10-19 11:05:24 +0800

    connect server to appllo client

--- src/examples/example1/index.js
+++ src/examples/example1/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 
 import { LineContainer } from '@xinghunm/widgets';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider, graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { HttpLink } from 'apollo-link-http';
 import { cardsListQuery } from './models/local';
 import CardsList from '../../views/cards_list';
-import { mockNetworkInterface } from './mock/mocks';
 
+
+const httpLink = new HttpLink({ 
+  uri: 'http://localhost:8080/graphql'
+});
 const client = new ApolloClient({
-  networkInterface: mockNetworkInterface, 
+  link: httpLink,
+  cache: new InMemoryCache()
 });
 
 // client
@@ -41,3 +47,5 @@ class App extends Component {
     );
   }
 }
+
+export default App;
